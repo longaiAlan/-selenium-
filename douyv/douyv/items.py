d933ebcac86d5f7e0537389c41b194594438179f
longaiAlan <113747482+longaiAlan@users.noreply.github.com>
2023-04-28 00:58:09 +0800

    Update items.py

--- douyv/douyv/items.py
+++ douyv/douyv/items.py
@@ -9,7 +9,7 @@ import scrapy
 class DouyvItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    # 主播名字
+    # 主播名
     nickname = scrapy.Field()
-    # 封面图片
+    # 图片
     verticalSrc = scrapy.Field()
